3fb3271c4113728b196ac682dfc6def2b181aa2f
Aashray11Arora <aashray11arora@gmail.com>
2024-08-01 14:52:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,26 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // Middleware setup
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Change '*' to your specific origin(s) for better security
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // Adjust path as needed
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Change '*' to your specific origin(s) for better security
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
+});
 
 // Session configuration
 app.use(session({
@@ -58,6 +70,7 @@ app.get('/check-variable', (req, res) => {
 
 // API route for data
 app.get('/api/data', async (req, res) => {
+
   try {
     const query = (req.query.q || '').toLowerCase();
     const status = req.query.status || '';
